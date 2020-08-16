a58d3e319eacf1ebe8128b3ebc64f883cc9dc109
daliborstakic <stakicdalibor64@gmail.com>
2020-08-16 09:26:04 +0200

    added contact route

--- perswebs/routes.py
+++ perswebs/routes.py
@@ -11,3 +11,7 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact')
