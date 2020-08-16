a1caaea3cce542471a6d8f79e7c4422266d14068
daliborstakic <stakicdalibor64@gmail.com>
2020-08-16 15:04:11 +0200

    removed contact and about routes

--- perswebs/routes.py
+++ perswebs/routes.py
@@ -6,12 +6,3 @@ from perswebs import app
 @app.route('/home')
 def home():
     return render_template('home.html')
-
-# About page
-@app.route('/about')
-def about():
-    return render_template('about.html', title='About')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html', title='Contact')
